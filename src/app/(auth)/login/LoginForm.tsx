bb68244a195dfe8f94310a097bc63a14e955fae2
Edison.Cheung <edison.cheung@192.168.1.100>
2025-11-16 15:36:18 +0800

    style: 登陆页面

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import TextDivider from '@/components/TextDivider'
-import { Button, Input, Link } from '@heroui/react'
+import { Button, Checkbox, Input, Link } from '@heroui/react'
 import { useFormik } from 'formik'
-import { Lock, Phone } from 'lucide-react'
+import { Fingerprint, Lock, Phone } from 'lucide-react'
 import * as Yup from 'yup'
 
 const validationSchema = Yup.object({
@@ -15,7 +15,8 @@ export default function LoginForm() {
   const formik = useFormik({
     initialValues: {
       phone: '',
-      password: ''
+      password: '',
+      rememberMe: false
     },
     validationSchema,
     onSubmit: async values => {
@@ -36,32 +37,62 @@ export default function LoginForm() {
         isInvalid={!!(formik.touched.phone && formik.errors.phone)}
         startContent={<Phone size={18} />}
       />
-      <Input
-        label={formik.touched.password && formik.errors.password ? formik.errors.password : '密码'}
-        name="password"
-        type="password"
-        labelPlacement="outside-top"
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        isInvalid={!!(formik.touched.password && formik.errors.password)}
-        startContent={<Lock size={18} />}
-      />
-      <div className="flex justify-end mt-5">
-        <Link color="primary" className="cursor-pointer">
-          忘记密码 ?
-        </Link>
+      <div>
+        <div className="flex justify-between items-center mb-2">
+          <label
+            className={`text-sm ${formik.touched.password && formik.errors.password ? 'text-danger' : 'text-foreground'}`}
+          >
+            {formik.touched.password && formik.errors.password ? formik.errors.password : '密码'}
+          </label>
+          <Link color="primary" className="cursor-pointer text-sm">
+            忘记密码 ?
+          </Link>
+        </div>
+        <Input
+          name="password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          isInvalid={!!(formik.touched.password && formik.errors.password)}
+          startContent={<Lock size={18} />}
+        />
       </div>
+      <Checkbox
+        name="rememberMe"
+        isSelected={formik.values.rememberMe}
+        onValueChange={value => formik.setFieldValue('rememberMe', value)}
+        size="sm"
+      >
+        记住我
+      </Checkbox>
       <Button
         type="submit"
         color="primary"
         variant="shadow"
         fullWidth
         isLoading={formik.isSubmitting}
+        className="mt-2"
       >
-        登录
+        登 录
       </Button>
-      <TextDivider text="其他登录方式" className="my-6" />
+      <div className="text-center text-sm text-gray-600">
+        还没有账号？
+        <Link href="#" underline="always" className="ml-1 text-sm">
+          请注册
+        </Link>
+      </div>
+      <TextDivider text="其他登录方式" />
+      <Button
+        variant="flat"
+        radius="full"
+        color="success"
+        fullWidth
+        isDisabled
+        startContent={<Fingerprint size={18} />}
+      >
+        使用微信登录
+      </Button>
     </form>
   )
 }

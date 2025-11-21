fb192f2949a77e095d335f1cd4526da6c944861a
Edison.Cheung <edison.cheung@192.168.1.100>
2025-11-21 22:48:19 +0800

    chore: disable source maps

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: { position: 'bottom-right' }
+  devIndicators: { position: 'bottom-right' },
+  // disable source maps in production for better security and performance
+  productionBrowserSourceMaps: false,
+  experimental: {
+    serverSourceMaps: false
+    // webpackMemoryOptimizations: false
+  }
 }
 
 export default nextConfig
